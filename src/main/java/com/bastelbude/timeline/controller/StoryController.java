d70501bd14faaa81baa9054d822ff956b55da6c7
wbiker <wbiker@mailbox.org>
2026-02-28 15:18:31 +0100

    save story endpoint

--- src/main/java/com/bastelbude/timeline/controller/StoryController.java
+++ src/main/java/com/bastelbude/timeline/controller/StoryController.java
@@ -4,6 +4,8 @@ import com.bastelbude.timeline.model.StoryModel;
 import com.bastelbude.timeline.services.StoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,5 +19,10 @@ public class StoryController {
   @GetMapping("/stories")
   public List<StoryModel> getStories() {
     return storyService.findAll();
+  }
+
+  @PostMapping("/stories")
+  public StoryModel storeStory(@RequestBody StoryModel storyModel) {
+    return storyService.save(storyModel);
   }
 }
--- src/main/java/com/bastelbude/timeline/services/StoryService.java
+++ src/main/java/com/bastelbude/timeline/services/StoryService.java
@@ -1,5 +1,6 @@
 package com.bastelbude.timeline.services;
 
+import com.bastelbude.timeline.entities.Story;
 import com.bastelbude.timeline.model.StoryModel;
 import com.bastelbude.timeline.repositories.StoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,5 +18,15 @@ public class StoryService {
     return repository.findAll().stream()
       .map(storyEntity -> new StoryModel(storyEntity.getIdentifier(), storyEntity.getStoryId(), storyEntity.getTitle()))
       .toList();
+  }
+
+  public StoryModel save(StoryModel storyModel) {
+    Story storyEntity = new Story(storyModel.id(), storyModel.storyId(), storyModel.title());
+    var entitySaved = repository.save(storyEntity);
+    return StoryModel.builder()
+            .id(entitySaved.getIdentifier())
+            .storyId(entitySaved.getStoryId())
+            .title(entitySaved.getTitle())
+            .build();
   }
 }
