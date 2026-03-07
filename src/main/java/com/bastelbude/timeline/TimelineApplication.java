d52e66b5bbdcfce04b033d7b8feb4fc60177988b
wbiker <wbiker@mailbox.org>
2026-03-07 18:39:26 +0100

    smaller improvements

--- src/main/java/com/bastelbude/timeline/TimelineApplication.java
+++ src/main/java/com/bastelbude/timeline/TimelineApplication.java
@@ -3,6 +3,9 @@ package com.bastelbude.timeline;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 public class TimelineApplication {
@@ -11,4 +14,15 @@ public class TimelineApplication {
 		SpringApplication.run(TimelineApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("http://localhost:4200")
+						.allowedMethods("Get", "POST", "PUT", "DELETE");
+			}
+		};
+	}
 }
--- src/main/java/com/bastelbude/timeline/controller/StoryController.java
+++ src/main/java/com/bastelbude/timeline/controller/StoryController.java
@@ -4,11 +4,12 @@ import com.bastelbude.timeline.mapper.StoryMapper;
 import com.bastelbude.timeline.model.StoryModel;
 import com.bastelbude.timeline.services.StoryService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -27,8 +28,19 @@ public class StoryController {
                        .toList();
   }
 
-  @PostMapping("/stories")
+  @PostMapping(value="/stories", consumes="application/json", produces="application/json")
+  @ResponseBody
   public StoryModel storeStory(@RequestBody StoryModel storyModel) {
     return storyMapper.toModel(storyService.save(storyModel));
+  }
+
+  @Bean
+  UrlBasedCorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.setAllowedOrigins(Arrays.asList("https://example.com"));
+    configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
   }
 }
